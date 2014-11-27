ef8bc83459abd205fcdcd6257100f6c2749c8f41
Andrew Rota <rota.andrew@gmail.com>
2014-11-27 21:44:31 +0000

    Add more cities

--- src/js/views/coreMenuView.js
+++ src/js/views/coreMenuView.js
@@ -19,6 +19,52 @@ module.exports = BaseView.extend({
         lat: '40.7127',
         long: '-74.0059',
         zoom: 10
+    },
+    {
+        name: 'Chicago',
+        lat: '41.8369',
+        long: '-87.6847',
+        zoom: 10
+    },{
+        name: 'Cincinnati',
+        lat: '39.1000',
+        long: '-84.5167',
+        zoom: 10
+    },{
+        name: 'St. Louis',
+        lat: '38.6272',
+        long: '-90.1978',
+        zoom: 10
+    },{
+        name: 'Pittsburgh',
+        lat: '40.4417',
+        long: '-80.0000',
+        zoom: 10
+    },{
+        name: 'New Orleans',
+        lat: '29.9667',
+        long: '-90.0500',
+        zoom: 10
+    },{
+        name: 'Austin',
+        lat: '30.2500',
+        long: '-97.7500',
+        zoom: 10
+    },{
+        name: 'San Francisco',
+        lat: '37.7833',
+        long: '-122.4167',
+        zoom: 10
+    },{
+        name: 'Portland',
+        lat: '45.5200',
+        long: '-122.6819',
+        zoom: 10
+    },{
+        name: 'Honolulu',
+        lat: '21.3000',
+        long: '-157.8167',
+        zoom: 10
     }]),
     postRender: function() {
       // @TODO refactor selected city to be driven by events
