7569216fb3b70b0786ed5aa1460c3bc035884ca9
Andrew Rota <rota.andrew@gmail.com>
2014-11-27 21:25:02 +0000

    Add show city functionality

--- src/js/views/coreMenuView.js
+++ src/js/views/coreMenuView.js
@@ -2,8 +2,13 @@
 var BaseView = require('base/baseView.js');
 var Backbone = require('backbone');
 
+var messageBus = require('messageBus.js');
+
 module.exports = BaseView.extend({
     tagName: 'core-menu',
+    events: {
+        'click paper-item': 'handleClickItem'
+    },
     collection: new Backbone.Collection([{
         name: 'Boston',
         lat: '42.3581',
@@ -14,5 +19,10 @@ module.exports = BaseView.extend({
         lat: '40.7127',
         long: '-74.0059',
         zoom: 10
-    }])
+    }]),
+    handleClickItem: function(event) {
+        messageBus.trigger('showCity', this.collection.findWhere({
+            name: event.currentTarget.getAttribute('data-item-id')
+        }));
+    }
 });
--- src/js/views/googleMapView.js
+++ src/js/views/googleMapView.js
@@ -1,6 +1,17 @@
 'use strict';
+var _ = require('underscore');
 var BaseView = require('base/baseView.js');
+var messageBus = require('messageBus.js');
 
 module.exports = BaseView.extend({
-    tagName: 'google-map'
+    postInitialize: function() {
+        this.listenTo(messageBus, 'showCity', _.bind(this.showCity, this));
+    },
+    tagName: 'google-map',
+    showCity: function(model) {
+        this.model = model;
+        this.el.setAttribute('latitude', this.model.get('lat'));
+        this.el.setAttribute('longitude', this.model.get('long'));
+        this.el.setAttribute('zoom', this.model.get('zoom'));
+    }
 });
